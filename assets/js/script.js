8d57221df6f53059e9c70e885a86dd3eb690cbae
Joe Ekiert <joe.ekiert@gmail.com>
2013-09-11 00:49:54 -0700

    scroll to top gracefully on tab show (and only if not already at the top)

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,8 @@ $(document).ready(function () {
     $('.nav-pills').find('li').eq(1).addClass('active');
   });
 
+  $('a[data-toggle="tab"]').on('show', function () {
+    if ($(window).scrollTop())
+      $('html, body').animate({scrollTop:0}, 'fast');
+  });
 });
