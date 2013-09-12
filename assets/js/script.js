4d8ccbf36856c90ecef2bfb71cdcd5237c9f4f72
Joe Ekiert <joe.ekiert@gmail.com>
2013-09-12 00:25:46 -0700

    adding testimonials!  randomizer everytime you load the page or switch back to home - may add to all tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var testimonials;
+
 $(document).ready(function () {
   $(document).on('click', '#start-tour', function () {
     $('.nav-pills').find('li').removeClass('active');
@@ -8,4 +10,31 @@ $(document).ready(function () {
     if ($(window).scrollTop())
       $('html, body').animate({scrollTop:0}, 'fast');
   });
+
+  loadTesties();
+
 });
+
+function loadTesties() {
+  $.getJSON('assets/js/testimonials.json', function(data) {
+    testimonials = data;
+    triggerNewTesti();
+    $('a[href="#home"]').on('show', function () {
+      triggerNewTesti()
+    });
+  });
+}
+
+function triggerNewTesti() {
+  var _person = randomize(0, testimonials.length - 1);
+  var _quote = randomize(0, testimonials[_person].testimonials.length - 1);
+
+  $('blockquote').find('p').text(testimonials[_person].testimonials[_quote]);
+  $('blockquote').find('small').text(testimonials[_person].name);
+  $('blockquote').show();
+}
+
+function randomize(min, max) {
+  var random = Math.floor((Math.random() * ((max + 1) - min)) + min);
+  return random;
+}
